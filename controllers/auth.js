b9bafe2b1178876d98bcdfb610f63b96ad13dc36
hotgorod <hotgorod@gmail.com>
2023-12-20 20:20:51 +0000

    register route added, 409 conflict added

---
+++ controllers/auth.js
@@ -0,0 +1,23 @@
+const { User } = require('../models/user');
+
+const { HttpError, ctrlWrapper } = require("../helpers");
+
+const register = async (req, res) => {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user) {
+        throw HttpError(409, "Email in use")
+    }
+
+    const newUser = await User.create(req.body);
+
+    res.status(201).json({
+      email: newUser.email,
+      subscription: "starter",
+    });
+};
+
+module.exports = {
+    register: ctrlWrapper(register),
+}
