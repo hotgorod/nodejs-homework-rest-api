2151fc438135d7971c53043046a134fbb71686e4
hotgorod <hotgorod@gmail.com>
2023-12-23 12:32:11 +0000

    token

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,7 +3,10 @@ const { Contact } = require("../models/contact");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const listContacts = async (req, res) => {
-  const result = await Contact.find();
+  const { _id: owner } = req.user;
+  
+  const result = await Contact.find({ owner }).populate("owner", "name email");
+  
   res.json(result);
 };
 
@@ -17,7 +20,8 @@ const getContactById = async (req, res) => {
 };
 
 const addContact = async (req, res) => {
-  const result = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
@@ -62,5 +66,5 @@ module.exports = {
   addContact: ctrlWrapper(addContact),
   updateContact: ctrlWrapper(updateContact),
   updateFavorite: ctrlWrapper(updateFavorite),
-    deleteContact: ctrlWrapper(deleteContact),
+  deleteContact: ctrlWrapper(deleteContact),
 };
