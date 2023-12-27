a4ab8ed5291021ac87c1a785a502120084a46ce9
hotgorod <hotgorod@gmail.com>
2023-12-27 10:04:44 +0000

    object user return

--- controllers/auth.js
+++ controllers/auth.js
@@ -21,8 +21,7 @@ const register = async (req, res) => {
     const newUser = await User.create({...req.body, password: hashPassword});
 
     res.status(201).json({
-      email: newUser.email,
-      subscription: "starter",
+      user: { email: newUser.email, subscription: "starter" },
     });
 };
 
