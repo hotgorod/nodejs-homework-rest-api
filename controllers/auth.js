16110800b18bfd42189800a1d063315cc5fa5e9c
hotgorod <hotgorod@gmail.com>
2024-01-10 17:27:39 +0000

    verify email sent successfully

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,15 +2,16 @@ const bcrypt = require("bcrypt");
 
 const { User } = require("../models/user");
 
-const { HttpError, ctrlWrapper } = require("../helpers");
+const { HttpError, ctrlWrapper, sendEmail } = require("../helpers");
 
-const { SECRET_KEY } = process.env;
+const { SECRET_KEY, BASE_URL } = process.env;
 
 const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
 const Jimp = require("jimp");
+const { nanoid } = require("nanoid");
 
 const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
@@ -24,13 +25,23 @@ const register = async (req, res) => {
 
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationToken = nanoid();
 
   const newUser = await User.create({
     ...req.body,
     password: hashPassword,
     avatarURL,
+    verificationToken,
   });
 
+    const verifyEmail = {
+      to: email,
+      subject: "Verify email",
+      html: `<a target="_blank" href="${BASE_URL}/users/verify/${verificationToken}">Click verify email</a>`,
+    };
+
+    await sendEmail(verifyEmail);
+    
   res.status(201).json({
     user: { email: newUser.email, subscription: "starter" },
   });
@@ -79,12 +90,12 @@ const logout = async (req, res) => {
 };
 
 const updateAvatar = async (req, res) => {
-    const { _id } = req.user;
-    if (!req.file) {
-      return res.status(400).send("No file uploaded.");
-    }
+  const { _id } = req.user;
+  if (!req.file) {
+    return res.status(400).send("No file uploaded.");
+  }
   const { path: tempUpload, originalname } = req.file;
-  
+
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
   await fs.rename(tempUpload, resultUpload);
